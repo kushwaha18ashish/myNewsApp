94095cbdfdd84c94be7bb78d7ea856b3f49369cf
kushwaha18ashish <ashish725494@gmail.com>
2025-03-10 21:05:46 +0530

    Till headlineFragment

--- app/src/main/java/com/mjolnir/newsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/mjolnir/newsapp/ui/fragments/ArticleFragment.kt
@@ -5,19 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
+import androidx.navigation.fragment.navArgs
 import com.example.newsprojectpractice.R
+import com.example.newsprojectpractice.databinding.FragmentArticleBinding
+import com.google.android.material.snackbar.Snackbar
+import com.mjolnir.newsapp.ui.NewsActivity
+import com.mjolnir.newsapp.ui.NewsViewModel
 
 
-class ArticleFragment : Fragment() {
+class ArticleFragment : Fragment(R.layout.fragment_article) {
 
+    lateinit var newsViewModel: NewsViewModel
+    val args : ArticleFragmentArgs by navArgs()
+    lateinit var binding: FragmentArticleBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_article, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding=FragmentArticleBinding.bind(view)
+
+        newsViewModel=(activity as NewsActivity).newsViewModel
+        val article=args.article
+
+        binding.webView.apply {
+            webViewClient= WebViewClient()
+            article.url?.let {
+                loadUrl(it)
+            }
+        }
+
+        binding.fab.setOnClickListener {
+            newsViewModel.addToFavourites(article)
+            Snackbar.make(view,"Added To Favorites",Snackbar.LENGTH_SHORT).show()
+        }
     }
+
 
 
 }
--- app/src/main/java/com/mjolnir/newsapp/ui/fragments/SearchFragment.kt
+++ app/src/main/java/com/mjolnir/newsapp/ui/fragments/SearchFragment.kt
@@ -10,13 +10,6 @@ import com.example.newsprojectpractice.R
 
 class SearchFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
-    }
 
 
 }
